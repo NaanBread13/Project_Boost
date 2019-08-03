7b3539cfa315fe1a3db9c692d828dc1bb45f4d44
Ronan Binny <binnyronan@gmail.com>
2019-08-03 18:10:43 +1200

    Made the movement of the obsatcle actually oscillate. Made the rocketship's thrust movement frame-rate independent

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f,10f,10f);
+    [SerializeField] float period = 2f;
     //remove from inspector
     [Range(0,1)] [SerializeField] float movementFactor; //0 for not moved, 1 for fully moved
 
@@ -19,6 +20,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //to do protect against zero
+        float cycles = Time.time / period; // gorws continually from zero
+
+        const float tau = Mathf.PI * 2; // about 6.28
+        float rawSinWave = Mathf.Sin(tau * cycles);
+
+        movementFactor = (rawSinWave / 2f) + 0.5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -111,7 +111,7 @@ public class Rocket : MonoBehaviour
 
     private void ApplyThrust()
     {
-        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
         if (!audioSource.isPlaying)
         { //so it doesn't layer audio
             audioSource.PlayOneShot(mainEngine);
